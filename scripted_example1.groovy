6ae92c55a7c4299ca60cff3145173a09be666576
Marcin Masłowski <marcin.maslowski@motorolasolutions.com>
2020-09-10 11:05:22 +0200

    Init

---
+++ scripted_example1.groovy
@@ -0,0 +1,14 @@
+
+
+node {
+    stage('Build') {
+        if (env.BRANCH_NAME == 'master') {
+            echo 'Build master version'
+            def version=build()
+            echo "Promote to artifactory"
+            promoteArtifactory(version)
+        } else {
+            echo 'I execute elsewhere'
+        }
+    }
+}
