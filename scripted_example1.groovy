01a19d4ecd461f7dd5889efe048fdac32064a9e6
Marcin Maslowski <KXM347@motorolasolutions.com>
2020-09-10 11:26:26 +0200

    Fix

--- scripted_example1.groovy
+++ scripted_example1.groovy
@@ -1,5 +1,4 @@
 
-
 node {
     stage('Build') {
         if (env.BRANCH_NAME == 'master') {
@@ -11,12 +10,4 @@ node {
             echo 'I execute elsewhere'
         }
     }
-}
-
-def build(){
-    echo "Build"
-}
-
-def promoteArtifactory(version){
-    echo "Promoting artifactory ${version}"
 }
